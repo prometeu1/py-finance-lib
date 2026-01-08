48be3baebd3a2688d467da3c44cf123a7cee1099
Kevin <kevin@example.com>
2026-01-08 15:16:34 +0100

    add asset managment and portofolio calculation

--- finance_lib/portfolio.py
+++ finance_lib/portfolio.py
@@ -1,3 +1,10 @@
 class Portfolio:
     def __init__(self):
         self.actions = []
+
+    def add_asset(self, action):
+        self.actions.append(action)
+
+    def get_total_value(self):
+        total = sum(action.total_value for action in self.actions)
+        return round(total, 2)
